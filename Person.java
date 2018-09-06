65d1ac6d6dd1be97b795055465d5422b809b1ad4
Rajan Khullar <rkhullar03@gmail.com>
2018-09-06 19:14:32 -0400

    person demo

---
+++ Person.java
@@ -0,0 +1,43 @@
+class Person extends Object
+{
+    // this class is immutable
+
+    private String name;
+    private int age;
+
+    public Person()
+    {
+//       name = null;
+//       age = 0;
+
+        this(null, 0);
+
+    }
+
+    public Person(String name)
+    {
+        this(name, 0);
+    }
+
+    public Person(String name, int age)
+    {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+
+    public int getAge()
+    {
+        return age;
+    }
+
+    public String toString()
+    {
+        return "Person: " + name + ", " + age;
+    }
+
+}
---
+++ PersonDemo.java
@@ -0,0 +1,14 @@
+class PersonDemo
+{
+    public static void main(String[] args)
+    {
+        Person[] people = new Person[3];
+
+        people[0] = new Person();
+        people[1] = new Person("Rajan");
+        people[2] = new Person("Smith", 23);
+
+        for(Person person: people)
+            System.out.println(person);
+    }
+}
